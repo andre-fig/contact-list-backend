f2503b786f984edb37c02b4278f427405471406f
André Figueiredo <afigueiredo@tasken.com>
2023-02-19 16:48:05 -0300

    hello world

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger/dist/decorators';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +7,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Hello World' })
+  @ApiResponse({ status: 200, description: 'Hello World!' })
   getHello(): string {
     return this.appService.getHello();
   }
